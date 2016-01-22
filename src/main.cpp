f2ad398fcd5f13a0c700de7a6fa724ec661f1eea
Mark Friedenbach <mark@friedenbach.org>
2016-01-22 08:56:14 -0800

    Switch to fast Merkle trees for witness.

--- src/main.cpp
+++ src/main.cpp
@@ -3403,7 +3403,7 @@ std::vector<unsigned char> GenerateCoinbaseCommitment(CBlock& block, const CBloc
     }
     if (fHaveWitness && IsWitnessEnabled(pindexPrev, consensusParams)) {
         if (commitpos == -1) {
-            uint256 witnessroot = BlockWitnessMerkleRoot(block, NULL);
+            uint256 witnessroot = BlockWitnessMerkleRoot(block);
             CTxOut out;
             out.nValue = 0;
             out.scriptPubKey.resize(39);
@@ -3506,8 +3506,7 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
             }
         }
         if (commitpos != -1) {
-            bool malleated = false;
-            uint256 hashWitness = BlockWitnessMerkleRoot(block, &malleated);
+            uint256 hashWitness = BlockWitnessMerkleRoot(block);
             // The malleation check is ignored; as the transaction tree itself
             // already does not permit it, it is impossible to trigger in the
             // witness tree.
