d9724664446d9ba5650c1bc85027481f8cbd28e5
Pieter Wuille <pieter.wuille@gmail.com>
2019-04-02 13:38:05 -0700

    Keep spent outputs in PrecomputedTransactionData during validation

--- src/validation.cpp
+++ src/validation.cpp
@@ -1508,11 +1508,17 @@ bool CheckInputs(const CTransaction& tx, TxValidationState &state, const CCoinsV
         return true;
     }
 
-    txdata.Init(tx);
+    std::vector<CTxOut> spent_outputs;
+    spent_outputs.reserve(tx.vin.size());
     for (unsigned int i = 0; i < tx.vin.size(); i++) {
         const COutPoint &prevout = tx.vin[i].prevout;
         const Coin& coin = inputs.AccessCoin(prevout);
         assert(!coin.IsSpent());
+        spent_outputs.emplace_back(coin.out);
+    }
+
+    txdata.Init(tx, std::move(spent_outputs));
+    for (unsigned int i = 0; i < tx.vin.size(); i++) {
 
         // We very carefully only pass in things to CScriptCheck which
         // are clearly committed to by tx' witness hash. This provides
@@ -1521,7 +1527,7 @@ bool CheckInputs(const CTransaction& tx, TxValidationState &state, const CCoinsV
         // spent being checked as a part of CScriptCheck.
 
         // Verify signature
-        CScriptCheck check(coin.out, tx, i, flags, cacheSigStore, &txdata);
+        CScriptCheck check(txdata.m_spent_outputs[i], tx, i, flags, cacheSigStore, &txdata);
         if (pvChecks) {
             pvChecks->push_back(CScriptCheck());
             check.swap(pvChecks->back());
@@ -1535,7 +1541,7 @@ bool CheckInputs(const CTransaction& tx, TxValidationState &state, const CCoinsV
                 // splitting the network between upgraded and
                 // non-upgraded nodes by banning CONSENSUS-failing
                 // data providers.
-                CScriptCheck check2(coin.out, tx, i,
+                CScriptCheck check2(txdata.m_spent_outputs[i], tx, i,
                         flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, cacheSigStore, &txdata);
                 if (check2())
                     return state.Invalid(TxValidationResult::TX_NOT_STANDARD, strprintf("non-mandatory-script-verify-flag (%s)", ScriptErrorString(check.GetScriptError())));
