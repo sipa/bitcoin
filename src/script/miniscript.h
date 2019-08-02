1c830cc6f9c8f35e60fb12991ffd68a49b26000d
Pieter Wuille <pieter.wuille@gmail.com>
2019-08-02 12:20:50 -0700

    bla

--- src/script/miniscript.h
+++ src/script/miniscript.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef _BITCOIN_SCRIPT_MINISCRIPT_H_
 #define _BITCOIN_SCRIPT_MINISCRIPT_H_ 1
 
