593d9f27f84ec84bc26022aaf3fa22802b58e350
Pieter Wuille <pieter@wuille.net>
2021-01-25 22:55:43 -0800

    Replace std::set based duplicate txin logic with sort/adjacent_find
    This only needs one allocation, and has far better memory locality.

--- src/consensus/tx_check.cpp
+++ src/consensus/tx_check.cpp
@@ -36,10 +36,24 @@ bool CheckTransaction(const CTransaction& tx, TxValidationState& state)
     // of a tx as spent, it does not check if the tx has duplicate inputs.
     // Failure to run this check will result in either a crash or an inflation bug, depending on the implementation of
     // the underlying coins database.
-    std::set<COutPoint> vInOutPoints;
-    for (const auto& txin : tx.vin) {
-        if (!vInOutPoints.insert(txin.prevout).second)
+    if (tx.vin.size() <= 3) {
+        for (size_t i = 0; i + 1 < tx.vin.size(); ++i) {
+            for (size_t j = i + 1; j < tx.vin.size(); ++j) {
+                if (tx.vin[i].prevout == tx.vin[j].prevout) {
+                    return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-inputs-duplicate");
+                }
+            }
+        }
+    } else {
+        std::vector<std::reference_wrapper<const COutPoint>> vec;
+        vec.reserve(tx.vin.size());
+        for (const auto& txin : tx.vin) {
+            vec.emplace_back(txin.prevout);
+        }
+        std::sort(vec.begin(), vec.end());
+        if (std::adjacent_find(vec.begin(), vec.end()) != vec.end()) {
             return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-inputs-duplicate");
+        }
     }
 
     if (tx.IsCoinBase())
