6307453b98647c15dd90ee45a7509b2788658aae
Pieter Wuille <pieter@wuille.net>
2021-02-08 00:15:51 -0800

    Add CKey::SignSchnorr function for BIP 340/341 signing

--- src/test/key_tests.cpp
+++ src/test/key_tests.cpp
@@ -302,4 +302,49 @@ BOOST_AUTO_TEST_CASE(bip340_test_vectors)
     }
 }
 
+BOOST_AUTO_TEST_CASE(bip340_test_vectors_signing)
+{
+    static const std::vector<std::array<std::string, 4>> VECTORS = {
+        {{"0000000000000000000000000000000000000000000000000000000000000003", "0000000000000000000000000000000000000000000000000000000000000000", "0000000000000000000000000000000000000000000000000000000000000000", "E907831F80848D1069A5371B402410364BDF1C5F8307B0084C55F1CE2DCA821525F66A4A85EA8B71E482A74F382D2CE5EBEEE8FDB2172F477DF4900D310536C0"}},
+        {{"B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF", "0000000000000000000000000000000000000000000000000000000000000001", "243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89", "6896BD60EEAE296DB48A229FF71DFE071BDE413E6D43F917DC8DCF8C78DE33418906D11AC976ABCCB20B091292BFF4EA897EFCB639EA871CFA95F6DE339E4B0A"}},
+        {{"C90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B14E5C9", "C87AA53824B4D7AE2EB035A2B5BBBCCC080E76CDC6D1692C4B0B62D798E6D906", "7E2D58D8B3BCDF1ABADEC7829054F90DDA9805AAB56C77333024B9D0A508B75C", "5831AAEED7B44BB74E5EAB94BA9D4294C49BCF2A60728D8B4C200F50DD313C1BAB745879A5AD954A72C45A91C3A51D3C7ADEA98D82F8481E0E1E03674A6F3FB7"}},
+        {{"0B432B2677937381AEF05BB02A66ECD012773062CF3FA2549E44F58ED2401710", "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "7EB0509757E246F19449885651611CB965ECC1A187DD51B64FDA1EDC9637D5EC97582B9CB13DB3933705B32BA982AF5AF25FD78881EBB32771FC5922EFC66EA3"}},
+    };
+
+    for (const auto& test : VECTORS) {
+        auto sec = ParseHex(test[0]);
+        auto aux = ParseHex(test[1]);
+        uint256 aux256(aux);
+        auto msg = ParseHex(test[2]);
+        uint256 msg256(msg);
+        auto sig = ParseHex(test[3]);
+        unsigned char sig64[64];
+
+        // Run the untweaked test vectors above, comparing with exact expected signature.
+        CKey key;
+        key.Set(sec.begin(), sec.end(), true);
+        XOnlyPubKey pubkey(key.GetPubKey());
+        bool ok = key.SignSchnorr(msg256, sig64, nullptr, &aux256);
+        BOOST_CHECK(ok);
+        BOOST_CHECK(std::vector<unsigned char>(sig64, sig64 + 64) == sig);
+        // Verify those signatures for good measure.
+        BOOST_CHECK(pubkey.VerifySchnorr(msg256, sig64));
+
+        // Do 10 iterations where we sign with a random Merkle root to tweak,
+        // and compare against the resulting tweaked keys, with random aux.
+        // In iteration i=0 we tweak with empty Merkle tree.
+        for (int i = 0; i < 10; ++i) {
+            uint256 merkle_root;
+            if (i) merkle_root = InsecureRand256();
+            auto tweaked = pubkey.CreateTapTweak(i ? &merkle_root : nullptr);
+            BOOST_CHECK(tweaked);
+            XOnlyPubKey tweaked_key = tweaked->first;
+            aux256 = InsecureRand256();
+            bool ok = key.SignSchnorr(msg256, sig64, &merkle_root, &aux256);
+            BOOST_CHECK(ok);
+            BOOST_CHECK(tweaked_key.VerifySchnorr(msg256, sig64));
+        }
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
