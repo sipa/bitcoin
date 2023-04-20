9711d22982d071bd9509eebadfeac80684f85643
Pieter Wuille <pieter@wuille.net>
2023-04-20 17:20:46 -0400

    Update limits

--- src/test/fuzz/memepool.cpp
+++ src/test/fuzz/memepool.cpp
@@ -1737,7 +1737,7 @@ FUZZ_TARGET(memepool_analyze_incexc_full)
 
     }
 
-    static constexpr size_t KNOWN_LIMITS[] = {0,  4, 17, 38, 72, 119, 177, 249, 335, 514,  770, 1183, 1834, 2818, 4181,  6440, 10123, 15128, 22948, 33482,  55482,  84737, 131446, 201394, 303571,  454081,  685079}; /* highest individual feerate first; num comparisons; full chunking */
+    static constexpr size_t KNOWN_LIMITS[] = {0,  4, 17, 38, 72, 119, 177, 249, 335, 514,  770, 1183, 1834, 2820, 4181,  6440, 10123, 15495, 22948, 33482,  55482,  84737, 131446, 201394, 303571,  454081,  685079}; /* highest individual feerate first; num comparisons; full chunking */
     /*                                        0   1   2   3   4    5    6    7    8    9    10    11    12    13    14     15     16     17     18     19      20      21      22      23      24       25       26 */
     /*                                        0   4   9  15  22   30   39   49   60   72    85    99   114   130   147    165    184    204    225    247     270     294     319     345     372      400      429 */
 
