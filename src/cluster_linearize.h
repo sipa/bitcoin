a4451088f8da7b3f7f1c66af38bb065f16aee95a
Pieter Wuille <pieter@wuille.net>
2024-05-08 18:09:34 -0400

    clusterlin: add SearchCandidateFinder class

--- src/cluster_linearize.h
+++ src/cluster_linearize.h
@@ -5,6 +5,8 @@
 #ifndef BITCOIN_CLUSTER_LINEARIZE_H
 #define BITCOIN_CLUSTER_LINEARIZE_H
 
+#include <algorithm>
+#include <numeric>
 #include <optional>
 #include <stdint.h>
 #include <vector>
@@ -304,6 +306,166 @@ public:
         }
         Assume(best.has_value());
         return {m_depgraph.Ancestors(*best) & m_todo, m_ancestor_set_feerates[*best]};
+    }
+};
+
+/** Class encapsulating the state needed to perform search for good candidate sets. */
+template<typename S>
+class SearchCandidateFinder
+{
+    /** Internal dependency graph for the cluster. */
+    const DepGraph<S>& m_depgraph;
+    /** Which transactions are left to do (sorted indices). */
+    S m_todo;
+
+public:
+    /** Construct a candidate finder for a graph.
+     *
+     * @param[in] depgraph   Dependency graph for the to-be-linearized cluster.
+     *
+     * Complexity: O(1).
+     */
+    SearchCandidateFinder(const DepGraph<S>& depgraph LIFETIMEBOUND) noexcept :
+        m_depgraph(depgraph),
+        m_todo(S::Fill(depgraph.TxCount())) {}
+
+    /** Find a high-feerate topologically-valid subset of what remains of the cluster.
+     *
+     * @param[in,out] iterations_left    On input, an upper bound on the number of optimization
+     *                                   steps that can be performed. On output, that number is
+     *                                   reduced by the number of actually performed optimization
+     *                                   steps.
+     * @param[in] best                   A set/feerate pair with an already-known good candidate.
+     *                                   This can be empty.
+     * @return                           The best (highest feerate, smallest size as tiebreaker)
+     *                                   topologically-valid subset of what remains of the cluster
+     *                                   that was encountered during search. If iterations_left is
+     *                                   nonzero on output, it is the absolute best such subset. If
+     *                                   not, the feerate of the returned set will be at least as
+     *                                   good as the best passed in.
+     *
+     * Complexity: possibly O(N * min(iterations_left, sqrt(2^N))) where N=depgraph.TxCount().
+     */
+    std::pair<S, FeeFrac> FindCandidateSet(uint64_t& iterations_left, std::pair<S, FeeFrac> best) noexcept
+    {
+        // Bail out quickly if we're given a (remaining) cluster that is empty.
+        if (m_todo.None()) return {};
+
+        if (best.second.IsEmpty()) {
+            // Set best to the entire remainder if not provided.
+            best.first = m_todo;
+            best.second = m_depgraph.FeeRate(m_todo);
+        }
+        Assume(!best.second.IsEmpty());
+        Assume(best.first.Any());
+        Assume(best.first << m_todo);
+
+        /** Type for work queue items. */
+        struct WorkItem
+        {
+            /** Set of transactions definitely included. This must be a subset of m_todo, and be
+             *  topologically valid (includes all in-m_todo ancestors of itself). */
+            S inc;
+            /** Set of undecided transactions. This must be a subset of m_todo, and have no overlap
+             *  with inc. The set (inc | und) must be topologically valid. */
+            S und;
+            /** Equal to m_depgraph.FeeRate(inc). */
+            FeeFrac inc_feerate;
+            /** Construct a new work item. */
+            WorkItem(S&& i, S&& u, FeeFrac&& i_f) noexcept :
+                inc(std::move(i)), und(std::move(u)), inc_feerate(std::move(i_f)) {}
+        };
+
+        /** The queue of work items. */
+        std::vector<WorkItem> queue;
+
+        /** Local copy of the iteration limit. */
+        uint64_t iteration_limit = iterations_left;
+
+        /** Internal function to add a work item, possibly improving it before doing so.
+         *
+         * - inc: the "inc" value for the new work item
+         * - und: the "und" value for the new work item
+         * - inc_feerate: equal to m_depgraph.FeeRate(inc)
+         */
+        auto add_fn = [&](S inc, S und, FeeFrac inc_feerate) noexcept {
+            Assume(inc << m_todo);
+            Assume(und << m_todo);
+            Assume(!(inc && und));
+
+            if (!inc_feerate.IsEmpty()) {
+                // If inc_feerate is better than best_feerate, remember inc as our new best.
+                if (inc_feerate > best.second) {
+                    best = {inc, inc_feerate};
+                }
+            }
+
+            // Make sure there are undecided transactions left to split on.
+            if (und.None()) return;
+
+            // Actually construct new work item on the queue.
+            queue.emplace_back(std::move(inc), std::move(und), std::move(inc_feerate));
+        };
+
+        /** Internal process function. It takes an existing work item, and splits it in two: one
+         *  with a particular transaction (and its ancestors) included, and one with that
+         *  transaction (and its descendants) excluded. */
+        auto split_fn = [&](WorkItem&& elem) noexcept {
+            // Any queue element must have undecided transactions left, otherwise there is nothing
+            // to explore anymore.
+            Assume(elem.und.Any());
+            // The included and undecided set are all subsets of m_todo.
+            Assume((m_todo >> elem.inc) && (m_todo >> elem.und));
+            // Included transactions cannot be undecided.
+            Assume(!(elem.inc && elem.und));
+            // We must have a non-empty best.
+            Assume(!best.second.IsEmpty());
+
+            const ClusterIndex first = elem.und.First();
+
+            // Add a work item corresponding to excluding the first undecided transaction.
+            const auto& desc = m_depgraph.Descendants(first);
+            add_fn(/*inc=*/elem.inc,
+                   /*und=*/elem.und / desc,
+                   /*inc_feefrac=*/elem.inc_feerate);
+
+            // Add a work item corresponding to including the first undecided transaction.
+            const auto anc = m_depgraph.Ancestors(first) & m_todo;
+            const auto new_inc = elem.inc | anc;
+            add_fn(/*inc=*/new_inc,
+                   /*und=*/elem.und / anc,
+                   /*inc_feefrac=*/elem.inc_feerate + m_depgraph.FeeRate(anc / elem.inc));
+
+            // Account for the performed split.
+            --iteration_limit;
+        };
+
+        // Create an initial entry for all of m_todo.
+        add_fn(/*inc=*/S{},
+               /*und=*/m_todo,
+               /*inc_feefrac=*/FeeFrac{});
+
+        // Work processing loop.
+        while (!queue.empty()) {
+            if (!iteration_limit) break;
+            auto elem = queue.back();
+            queue.pop_back();
+            split_fn(std::move(elem));
+        }
+
+        // Return what remains of the iteration limit.
+        iterations_left = iteration_limit;
+        // Return the found best set.
+        return best;
+    }
+
+    /** Remove a subset of transactions from the cluster being linearized.
+     *
+     * Complexity: O(N) where N=done.Count().
+     */
+    void MarkDone(const S& done) noexcept
+    {
+        m_todo /= done;
     }
 };
 
